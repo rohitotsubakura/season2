b23f84a2f13c0726765ca94eeacf1b9fbb22855f
Rohito Tsubakura <gardenia.k2012.zb.yi@gmail.com>
2020-03-18 13:09:02 +0900

    [add] stylesのファイルを追加

---
+++ src/styles/Typography.ts
@@ -0,0 +1,15 @@
+export default {
+    White: 'rgba(255, 255, 255, 1)',
+    Black: 'rgba(64, 55, 55, 1)',
+    BoundaryBlack: 'rgba(64, 55, 55, 0.16)',
+    BoundaryWhite: 'rgba(255, 255, 255, 0.16)',
+    Gray: 'rgba(160, 150, 150, 1)',
+    WhiteGray: 'rgba(249, 249, 249, 1)',
+    Primary: 'rgba(198, 82, 82, 1)',
+    Secondary: 'rgba(242, 229, 208, 1)',
+    BackToTop: '#d02c2c',
+    SnsTwitter: '#1da1f2',
+    SNSYouTube: '#ff0000',
+    SNSGitHub: '#333333',
+    SNSNote: '#6fcfbe'
+}
