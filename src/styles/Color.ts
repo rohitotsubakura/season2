291f98f7d8152e247808fccccf04ae631407e881
Rohito Tsubakura <gardenia.k2012.zb.yi@gmail.com>
2020-03-18 13:46:22 +0900

    [modify] 使う色を定義した

---
+++ src/styles/Color.ts
@@ -0,0 +1,14 @@
+export default {
+    Black: 'rgba(64, 55, 55, 1)',
+    White: 'rgba(255, 255, 255, 1)',
+    BoundaryBlack: 'rgba(64, 55, 55, 0.16)',
+    BoundaryWhite: 'rgba(255, 255, 255, 0.16)',
+    Gray: 'rgba(160, 150, 150, 1)',
+    Primary: 'rgba(198, 82, 82, 1)',
+    Secondary: 'rgba(242, 229, 208, 1)',
+    BackToTop: 'rgba(208, 44, 44, 1)',
+    SnsTwitter: '#1da1f2',
+    SnsYouTube: '#ff0000',
+    SnsGitHub: '#333333',
+    SnsNote: 'rgba(111, 207, 190, 1)'
+}
