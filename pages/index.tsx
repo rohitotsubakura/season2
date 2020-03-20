6593aa3571e32e44032aa133ea08a1a4c28375c5
Rohito Tsubakura <gardenia.k2012.zb.yi@gmail.com>
2020-03-20 12:41:47 +0900

    [modify] ページにグローバルスタイルを付与

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { NextPage, NextPageContext } from 'next';
+import Global from "../src/styles/Global";
 import Button from "../src/components/Button";
 import Footer from "../src/components/Footer";
 
 const Home: NextPage<{ userAgent:string }> = ({ userAgent }) => (
     <>
+        <Global />
         <h1>Hello Next! - user agent: {userAgent}</h1>
         <Button
             to='https://github.com/rohitotsubakura/season2'
